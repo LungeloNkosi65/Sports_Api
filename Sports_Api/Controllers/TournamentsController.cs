cf0af3c60297502bd8672091361503ae12225dc2
21711486@dut4life.ac.za <1965Lungelo#>
2020-03-20 16:56:56 +0200

    link format

--- Sports_Api/Controllers/TournamentsController.cs
+++ Sports_Api/Controllers/TournamentsController.cs
@@ -14,10 +14,10 @@ namespace Sports_Api.Controllers
     [ApiController]
     public class TournamentsController : ControllerBase
     {
-       
         [HttpGet]
         public IEnumerable<Tournament> Get(int? sportId,int ? countryId)
         {
+            //var a= "https://localhost:44365/api/tournaments?sportId=5&countryId=1";
             return AllLogic.GetTournaments(sportId,countryId).ToArray();
         }
     }
